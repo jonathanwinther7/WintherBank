4746cc231c7885dbe8dc1bad4a919f008c128996
jonathanwinther7 <jonathanwinther7@outlook.com>
2023-03-04 11:36:36 -0300

    fiz um registrador do total de contas que foram criadas aqui no WintherBank,
    É criei um cliente que é autenticavel por meio de um objeto anônimo criado via object expression.

--- src/main/kotlin/br/com/alura/WintherBank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/WintherBank/modelo/Conta.kt
@@ -1,11 +1,18 @@
 package br.com.alura.WintherBank.modelo
 
+var totalContas = 0
+    private set
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int,
 ) {
     var saldo = 0.0
         protected set
+    init {
+        println("Criando conta")
+        totalContas++
+    }
 
 
     fun deposita(valor: Double) {
--- src/main/kotlin/br/com/alura/WintherBank/teste/Main.kt
+++ src/main/kotlin/br/com/alura/WintherBank/teste/Main.kt
@@ -1,5 +1,28 @@
-fun main(){
+import br.com.alura.WintherBank.modelo.*
+
+var totalContas = 0
+    private set
+
+fun main() {
+
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "111.111.111-11"
+        override val senha: Int = 1000
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("nome do cliente ${fran.nome}")
+
+    val alex = Cliente(nome = "jonathan", cpf = "", senha = 1);
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000);
+    val contaCorrente = ContaCorrente(titular = alex, numero = 2000);
     testaContasDiferentes()
+    println("total de contas: ${totalContas}")
 }
 
 
